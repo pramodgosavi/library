8830888969da0968b0e15d942728441e033288b0
Pramod B Gosavi <pramodgosavi@users.noreply.github.com>
2020-12-08 14:57:04 +0530

    Update library.py

--- library.py
+++ library.py
@@ -1,15 +1,19 @@
-f=open("LibraryBooksDATA.txt","r")
+#open txt file in read mode
+f=open("file.txt","r")
+
+#initialize list as below
 Books=[]
 MissBook=[]
 RepeatBook=[]
+#all Books numbers are append in Books list
 for x in f:
    # print(x)
     Books.append(int(x))
-
+#range upto last Book Number
 for b in range(1,22302):
     if b not in Books:
         MissBook.append(b)
-
+#for Repeated book number up to last book number
 for c in range(1,22302):
     if Books.count(c) > 1:
         RepeatBook.append(c)
